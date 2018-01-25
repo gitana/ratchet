8b2b6e6b87285ac5ce1fbe6842490c0f7e763bf3
uzquiano <mgu1@cornell.edu>
2018-01-24 21:07:50 -0500

    Added error handler registry

---
+++ js/error/AbstractErrorHandler.js
@@ -0,0 +1,42 @@
+(function($) {
+    Ratchet.AbstractErrorHandler = Base.extend({
+
+        constructor: function()
+        {
+        },
+
+        /**
+         * EXTENSION_POINT
+         *
+         * @param err
+         * @returns {boolean}
+         */
+        canHandle: function(err)
+        {
+            return false;
+        },
+
+        /**
+         * EXTENSION_POINT
+         *
+         * @returns {number}
+         */
+        getOrder: function()
+        {
+            return 500;
+        },
+
+        /**
+         * EXTENSION_POINT
+         *
+         * @param err
+         * @returns {boolean}
+         */
+        handle: function(err, onClose)
+        {
+
+        }
+
+    });
+
+})(jQuery);
